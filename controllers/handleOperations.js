fac5b8f9f443db5aa80cc52138e8e6ecaa387165
Axel <axelfuh@gmail.com>
2018-11-09 15:40:13 +0100

    Update model dynamic name

--- controllers/handleOperations.js
+++ controllers/handleOperations.js
@@ -1,8 +1,10 @@
 const { handleSingle, handleMultiple } = require('./handleQuery')
 const models = require('../models')
 
+const name = req => req.path.split("/")[1]
+
 const getAll = async (req, res, next) => {
-  const Model = models[req.path.replace(/\//g, '')]
+  const Model = models[name(req)]
 
   const { count, results } = await handleMultiple(Model, req)
 
@@ -16,7 +18,7 @@ const getAll = async (req, res, next) => {
 }
 
 const getById = async (req, res) => {
-  const Model = models[req.path.replace(/(\/|\d|\[|\]|,)/g, '')]
+  const Model = models[name(req)]
 
   await handleSingle(Model, req.params.id, res)
 }
