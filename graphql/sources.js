3019e03c7e4a0e2ac8436010e9ed4a033d5ce5e4
Erick Jansen <janseniter@gmail.com>
2020-08-20 18:48:30 +0200

    add option to query multiple characters by multiple ids

--- graphql/sources.js
+++ graphql/sources.js
@@ -12,6 +12,11 @@ class Character extends RESTDataSource {
     const data = await this.get('/', { ...filter, page })
     return data
   }
+
+  async charactersByIds({ ids }) {
+    const data = await this.get('/' + ids)
+    return data
+  }
   async character({ id }) {
     const data = await this.get('/' + id)
     return data
